97ca4aaf8405ae8f9c9d7b84ef82f57ffe62cdb3
jlara11 <julyana.pd2@gmail.com>
2023-07-22 21:28:05 -0300

    Primeiro commit

---
+++ src/test/java/com/jlara/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlara.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
