41ae1adf91928705e97526105344ce31741e91ca
jlara11 <julyana.pd2@gmail.com>
2023-07-30 20:53:56 -0300

    metódo DELETE para Tecnico implementado!

--- src/main/java/com/jlara/helpdesk/services/TecnicoService.java
+++ src/main/java/com/jlara/helpdesk/services/TecnicoService.java
@@ -46,6 +46,15 @@ public class TecnicoService {
 		oldObj = new Tecnico(objDTO);
 		return repository.save(oldObj);
 	}
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+		if(obj.getChamados().size() > 0 ){
+			throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado !");
+		}
+		repository.deleteById(id);
+		
+	}
+
 
 
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
@@ -61,6 +70,7 @@ public class TecnicoService {
 		
 		
 	}
+
 
 
 }
