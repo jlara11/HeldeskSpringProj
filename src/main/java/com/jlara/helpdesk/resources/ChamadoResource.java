fc5c6c4968b5a458947995d44006a15969decbc8
jlara11 <julyana.pd2@gmail.com>
2023-07-30 23:57:06 -0300

    findById para Chamado

---
+++ src/main/java/com/jlara/helpdesk/resources/ChamadoResource.java
@@ -0,0 +1,27 @@
+package com.jlara.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jlara.helpdesk.domain.Chamado;
+import com.jlara.helpdesk.domain.dtos.ChamadoDTO;
+import com.jlara.helpdesk.services.ChamadoService;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+
+	@Autowired
+	private ChamadoService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
+		Chamado obj = service.findById(id);
+		return ResponseEntity.ok().body(new ChamadoDTO(obj));
+		
+	}
+}
---
+++ src/main/java/com/jlara/helpdesk/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package com.jlara.helpdesk.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jlara.helpdesk.domain.Chamado;
+import com.jlara.helpdesk.repositories.ChamadoRepository;
+import com.jlara.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ChamadoService {
+	
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		java.util.Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " +id));
+	}
+
+}
